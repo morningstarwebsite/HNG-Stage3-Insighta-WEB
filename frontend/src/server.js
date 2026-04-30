afda25be1bbd02892961d99da527a0a9f82a775f
umeh chiamaka blessing <morningstarwebsite@github.com>
2026-04-30 20:14:08 +0100

    bug(port): fix the port number

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,7 +3,7 @@ import { env } from "./config/env.js";
 
 const app = createApp();
 
-app.listen(env.port, () => {
+app.listen(env.port, "0.0.0.0", () => {
   // eslint-disable-next-line no-console
-  console.log(`Insighta portal listening on http://localhost:${env.port}`);
+  console.log(`Insighta portal listening on 0.0.0.0:${env.port}`);
 });
